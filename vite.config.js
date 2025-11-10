d021c5121071c1fd4cc11e951ba91eb58680ffe2
nakul-saini404 <nakul.saini404@gmail.com>
2025-11-10 18:21:05 +0530

    solve bug

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/E-coomerce-app/', // must match your GitHub repo name exactly
+  base: '/E-commerce-app/', // must match your GitHub repo name exactly
   plugins: [react()],
 })
 
