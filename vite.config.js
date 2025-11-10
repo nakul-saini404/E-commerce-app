3872bb8624de80d9568fdaf985459e36afa11789
nakul-saini404 <nakul.saini404@gmail.com>
2025-11-10 17:55:09 +0530

    Prepare For Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/E-coomerce-app/', // must match your GitHub repo name exactly
   plugins: [react()],
 })
+
